f2de7babc7ebc648703fb5160ba241a7a1304a84
09B22013 <09B22013@localhost>
2024-11-01 21:24:05 +0900

    delete astPrint

--- s2/parser/VariableNameNode.java
+++ s2/parser/VariableNameNode.java
@@ -1,5 +1,5 @@
 package enshud.s2.parser;
-// 変数名
+
 public class VariableNameNode extends NonTerminalNode {
     public void parse(Context context) throws SyntaxException {
         addChild(new TerminalNode(context.checkTerminalSymbol("SIDENTIFIER")));
