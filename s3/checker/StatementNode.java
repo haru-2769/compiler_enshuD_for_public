9a3cb30ed7a2279b4bf5c991191d898c3b77a353
09B22013 <09B22013@localhost>
2024-11-07 11:15:36 +0900

    delete token2,token3

--- s3/checker/StatementNode.java
+++ s3/checker/StatementNode.java
@@ -2,15 +2,13 @@ package enshud.s3.checker;
 
 public class StatementNode extends AstNode{
     private BasicStatementNode basicStatementNode;
-    Token token1, token2, token3;
+    Token token;
     private ExpressionNode expressionNode;
     private CompoundStatementNode compoundStatementNode1, compoundStatementNode2;
     public StatementNode() throws SyntaxException {
         this.basicStatementNode = null;
         this.expressionNode = null;
-        this.token1 = null;
-        this.token2 = null;
-        this.token3 = null;
+        this.token = null;
         this.compoundStatementNode1 = null;
         this.compoundStatementNode2 = null;
     }
@@ -20,22 +18,22 @@ public class StatementNode extends AstNode{
             this.basicStatementNode = new BasicStatementNode();
             this.basicStatementNode.parse(context);
         } else if (context.equalsAny(0, "SIF")) {
-            this.token1 = context.checkTerminalSymbol("SIF");
+            this.token = context.checkTerminalSymbol("SIF");
             this.expressionNode = new ExpressionNode();
             this.expressionNode.parse(context);
-            this.token2 = context.checkTerminalSymbol("STHEN");
+            context.checkTerminalSymbol("STHEN");
             this.compoundStatementNode1 = new CompoundStatementNode();
             this.compoundStatementNode1.parse(context);
             if (context.equalsAny(0, "SELSE")) {
-                this.token3 = context.checkTerminalSymbol("SELSE");
+                context.checkTerminalSymbol("SELSE");
                 this.compoundStatementNode2 = new CompoundStatementNode();
                 this.compoundStatementNode2.parse(context);
             }
         } else {
-            this.token1 = context.checkTerminalSymbol("SWHILE");
+            this.token = context.checkTerminalSymbol("SWHILE");
             this.expressionNode = new ExpressionNode();
             this.expressionNode.parse(context);
-            this.token2 = context.checkTerminalSymbol("SDO");
+            context.checkTerminalSymbol("SDO");
             this.compoundStatementNode1 = new CompoundStatementNode();
             this.compoundStatementNode1.parse(context);
         }
@@ -57,16 +55,8 @@ public class StatementNode extends AstNode{
         return this.compoundStatementNode2;
     }
 
-    public Token getToken1() {
-        return this.token1;
-    }
-
-    public Token getToken2() {
-        return this.token2;
-    }
-
-    public Token getToken3() {
-        return this.token3;
+    public Token getToken() {
+        return this.token;
     }
 
     public void accept(Visitor visitor) throws SemanticException {
