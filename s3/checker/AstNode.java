723f7b67d79056675919d6a029ce3cf341f17452
09B22013 <09B22013@localhost>
2024-12-10 17:01:38 +0900

    delete space

--- s3/checker/AstNode.java
+++ s3/checker/AstNode.java
@@ -2,5 +2,5 @@ package enshud.s3.checker;
 
 public abstract class AstNode {
 	public abstract void parse(Context context) throws SyntaxException;
-	public abstract  void accept(Visitor visitor) throws SemanticException;
+	public abstract void accept(Visitor visitor) throws SemanticException;
 }
